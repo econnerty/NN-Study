0a2561ec73f37d6d6b8fc555b43791ae4fbdadfa
Erik Connerty <erikc@email.sc.edu>
2023-06-25 01:00:27 -0400

    save tokenizer

--- Chat.py
+++ Chat.py
@@ -1,17 +1,15 @@
 # Erik Connerty
 # 6/24/2023
 # USC - AI institute
-from transformers import GPT2LMHeadModel, GPT2Tokenizer, pipeline
+from transformers import pipeline
 
-# Load the model and tokenizer
-model = GPT2LMHeadModel.from_pretrained("./Models/GPT2_TrainedModels")
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+pred_model = pipeline("fill-mask", model = "TrainedModels/GPT2_TrainedModels")
 
-# Create a text generation pipeline
-text_generator = pipeline("text-generation", model=model, tokenizer=tokenizer,pad_token_id = 50256)
+text = "This is an [MASK] movie."
 
-# Start a chat prompt
-while True:
-    prompt = input("You: ")
-    response = text_generator(prompt, max_length=200, do_sample=True, temperature=0.7)
-    print("Chatbot:", response[0]['generated_text'])
+preds = pred_model(text)
+
+for pred in preds:
+    print(f">>> {pred['sequence']}")
+
+
--- Transformer.py
+++ Transformer.py
@@ -1,12 +1,12 @@
 # Erik Connerty
 # 6/24/2023
 # USC - AI institute
-from transformers import GPT2LMHeadModel, GPT2Tokenizer, Trainer, TrainingArguments
+from transformers import AutoModelForCausalLM, AutoTokenizer, Trainer, TrainingArguments
 from datasets import load_dataset
 
 # Load and process dataset
 dataset = load_dataset("imdb")
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+tokenizer = AutoTokenizer.from_pretrained("gpt2")
 
 if tokenizer.pad_token is None:
     tokenizer.add_special_tokens({'pad_token': '[PAD]'})
@@ -22,7 +22,7 @@ tokenized_dataset = dataset.map(tokenize_function, batched=True)
 training_args = TrainingArguments(
     output_dir="./GPT2_TrainedModels",
     overwrite_output_dir=True,
-    num_train_epochs=3,
+    num_train_epochs=1,
     per_device_train_batch_size=1,  # batch size should be as large as you can afford on your hardware
     save_steps=10_000,
     save_total_limit=2,
@@ -30,7 +30,7 @@ training_args = TrainingArguments(
 )
 
 # Define trainer
-model = GPT2LMHeadModel.from_pretrained("gpt2").cuda()
+model = AutoModelForCausalLM.from_pretrained("gpt2").cuda()
 
 model.resize_token_embeddings(len(tokenizer))
 
@@ -45,4 +45,5 @@ trainer = Trainer(
 trainer.train()
 
 # Save the model
-model.save_pretrained("./GPT2_TrainedModels")
+model.save_pretrained("./TrainedModels/GPT2_TrainedModels")
+tokenizer.save_pretrained("./TrainedModels/GPT2_TrainedModels")
