2b72ec5ba9ec1e426859f5b63fc62eeed45c5822
Erik Connerty <erikc@email.sc.edu>
2023-06-24 22:55:38 -0400

    Transformer model

---
+++ Transformer.py
@@ -0,0 +1,45 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer, Trainer, TrainingArguments
+from datasets import load_dataset
+
+# Load and process dataset
+dataset = load_dataset("imdb")
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
+
+if tokenizer.pad_token is None:
+    tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+
+def tokenize_function(data):
+    result = tokenizer(data["text"], padding="max_length", truncation=True, max_length=128)
+    result["labels"] = result["input_ids"].copy()
+    return result
+
+tokenized_dataset = dataset.map(tokenize_function, batched=True)
+
+# Training Arguments
+training_args = TrainingArguments(
+    output_dir="./GPT2_TrainedModels",
+    overwrite_output_dir=True,
+    num_train_epochs=3,
+    per_device_train_batch_size=1,  # batch size should be as large as you can afford on your hardware
+    save_steps=10_000,
+    save_total_limit=2,
+    optim="adamw_torch",
+)
+
+# Define trainer
+model = GPT2LMHeadModel.from_pretrained("gpt2").cuda()
+
+model.resize_token_embeddings(len(tokenizer))
+
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_dataset["train"],
+    eval_dataset=tokenized_dataset["test"],
+)
+
+# Train the model
+trainer.train()
+
+# Save the model
+model.save_pretrained("./GPT-2-neo-imdb")
