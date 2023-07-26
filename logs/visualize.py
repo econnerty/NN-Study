da2eb42a72efb4903c6bf7ea61d600390fe99cf2
Erik Connerty <erikc@email.sc.edu>
2023-07-25 21:52:09 -0400

    updates

--- logs/visualize.py
+++ logs/visualize.py
@@ -58,11 +58,30 @@ for file in train_files:
     model_color = model_info[file_base]["color"]
     model_params = model_info[file_base]["params"]
 
-    # Add a trace to the figure
-    fig_train.add_trace(go.Scatter(x=average_loss.index, y=average_loss, mode='lines', name=f'{model_name} (Params: {model_params})', line=dict(color=model_color)))
+    if model_name in ['With Convolution: Small', 'With Convolution: Large']:
+        # If the model is one of the ones to highlight, add a 'border' line
+        fig_train.add_trace(go.Scatter(
+            x=average_loss.index,
+            y=average_loss,
+            mode='lines',
+            line=dict(color='black', width=5), # Set color and width of the 'border'
+            showlegend=False, # We don't want this trace to show up in the legend
+            hoverinfo='none' # Prevent hover text from showing up for this trace
+
+        ))
+
+    # Add the original line (which will be overlaid on the 'border' line if it exists)
+    fig_train.add_trace(go.Scatter(
+        x=average_loss.index,
+        y=average_loss,
+        mode='lines',
+        name=f'{model_name} (Params: {model_params})',
+        line=dict(color=model_color, width=2) # You may need to adjust the width depending on the 'border' width
+    ))
+
 
 # Update the layout of the figure
-fig_train.update_layout(title='Average Training Loss per Epoch for each Model (lower is better)', xaxis_title='Epoch', yaxis_title='Average Training Loss',hovermode='x unified')
+fig_train.update_layout(title='Average Training Loss per Epoch for each Model (lower is better)', xaxis_title='Epoch', yaxis_title="Training Loss (% of 100)",hovermode='x unified')
 
 # Loop over each test file
 for file in test_files:
@@ -75,32 +94,64 @@ for file in test_files:
     model_color = model_info[file_base]["color"]
     model_params = model_info[file_base]["params"]
 
-    # Add a trace to the figure
-    fig_test.add_trace(go.Scatter(x=df["Epoch"], y=df["Test Loss"], mode='lines',name=f'{model_name} (Params: {model_params})', line=dict(color=model_color)))
+    if model_name in ['With Convolution: Small', 'With Convolution: Large']:
+        # If the model is one of the ones to highlight, add a 'border' line
+        fig_test.add_trace(go.Scatter(
+            x=df["Epoch"],
+            y=df["Test Loss"],
+            mode='lines',
+            line=dict(color='black', width=5), # Set color and width of the 'border'
+            showlegend=False, # We don't want this trace to show up in the legend
+            hoverinfo='none' # Prevent hover text from showing up for this trace
+        ))
+
+    # Add the original line (which will be overlaid on the 'border' line if it exists)
+    fig_test.add_trace(go.Scatter(
+        x=df["Epoch"],
+        y=df["Test Loss"],
+        mode='lines',
+        name=f'{model_name} (Params: {model_params})',
+        line=dict(color=model_color, width=2) # You may need to adjust the width depending on the 'border' width
+    ))
 
 # Update the layout of the figure
-fig_test.update_layout(title='Test Loss per Epoch for each Model (lower is better)', xaxis_title='Epoch', yaxis_title='Test Loss', hovermode='x unified')
+fig_test.update_layout(title='Test Loss per Epoch for each Model (lower is better)', xaxis_title='Epoch', yaxis_title="Test Loss (% of 100)", hovermode='x unified')
 
 # Loop over each model
 for file_base, info in model_info.items():
+    if info["name"] in ['With Convolution: Small', 'With Convolution: Large']:
+        # If the model is one of the ones to highlight, give it a thick, dark border
+        line = dict(color='black', width=5)
+    else:
+        # Otherwise, give it no border
+        line = dict(width=0)
+    
     # Add a bar to the figure
-    fig_params.add_trace(go.Bar(x=[info["name"]], y=[info["params"]], name=info["name"], marker=dict(color=info["color"])))
+    fig_params.add_trace(go.Bar(
+        x=[info["name"]], 
+        y=[info["params"]], 
+        name=info["name"], 
+        marker=dict(
+            color=info["color"],
+            line=line
+        )
+    ))
 
 # Update the layout of the figure
-fig_params.update_layout(title='Parameter Count for each Model', xaxis_title='Model', yaxis_title='Parameter Count')
+fig_params.update_layout(title='Parameter Count for each Model', xaxis_title='Model', yaxis_title='Parameter Count (millions)')
 
 # Define the layout for your Dash app
 app.layout = html.Div(children=[
     html.H1(children='NN Fruit Classifier | With Convolution vs Without Convolution'),
 
     html.Div(children='''
-        A dashboard for visualizing training losses, test losses, and parameters count.
+        A dashboard for visualizing training loss, test loss, and parameter count.
     ''',style={'margin-bottom': '50px','font-weight': 'bold'}),
-    html.Img(src=app.get_asset_url('architecture.png'), style={'width': '100%', 'height': 'auto'}),
+    html.Img(src=app.get_asset_url('architecture.svg'), style={'width': '100%', 'height': 'auto'}),
     html.H2(children='What is Convolution?'),
     html.Div(children='''
-    Convolution is a technique for learning patterns in structured data. It can be thought of as passing a filter over an input data and learning
-    the best way to identify certain aspects of its inputs. It is best used when structure of the data is important.
+    Convolution is a technique for learning patterns in structured data. For the purpose of image recongnition, it can be thought of as passing several filters over an input image and learning
+    its most important features. It can be used anytime the input data needs a sense of spatial importance (I.E the data needs spatial information to be preserved).
 ''',style={'margin-bottom': '50px','font-weight': 'bold'}),
     html.Div(children='''
     A visualization of what a typical feed-forward neural network looks like. The input layer is the image of the fruit, the hidden layers are the neurons, and the output layer is the classification.
@@ -123,6 +174,7 @@ app.layout = html.Div(children=[
         id='example-graph-training',
         figure=fig_train
     ),
+        html.P('Note: Thicker lines are models that contain convolutional layers',style={'margin-bottom': '50px'}),
         html.Div(children='''
         The test loss as evaluated on a real word data set of 60 images taken from the internet. As we can see, training loss doesn't not always indicate good performance on real world
                  data. This is why we use a test set to evaluate our model. The non-convolutional models suffer from a phenomena known as overfitting.
@@ -132,6 +184,8 @@ app.layout = html.Div(children=[
         id='example-graph-test',
         figure=fig_test
     ),
+    html.P('Note: Thicker lines are models that contain convolutional layers',style={'margin-bottom': '50px'}),
+
     html.Div(children='''
         The smaller model won! It performed the best because it did not overfit the data and used convolution to learn patterns in the images more efficiently.
     ''',style={'font-weight': 'bold','font-size': '30px'}),
